0cbf03cab3668e264403dbc5676536bff87a644d
BreMea <63671187+BreMea@users.noreply.github.com>
2020-12-15 20:51:22 -0500

    add views and crap

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('fuck!');
+    res.render('app');
 });
 
 app.listen(3000, () => {
