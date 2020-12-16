e1c8bb6f93488e3efe1bcdda6cc6ba50028217ab
BreMea <63671187+BreMea@users.noreply.github.com>
2020-12-15 21:30:14 -0500

    get da queue

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const path = require('path');
+const getqueue = require('./methods/getqueue.js');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
-    console.log(await require('./methods/getqueue.js')());
-    res.render('app');
+    res.set('Content-Type', 'text/html');
+    res.send(fs.readFileSync(__dirname + '/queuescrape/index.html'));
 });
 
 app.listen(3000, () => {
--- methods/getqueue.js
+++ methods/getqueue.js
@@ -1,11 +1,20 @@
 const scrape = require('website-scraper');
 const fs = require('fs');
+const HTMLParser = require('node-html-parser');
 const rimraf = require('rimraf');
 
 module.exports = function() {
     return new Promise(async function(resolve, reject) {
         rimraf.sync('./queuescrape');
-        const queue = await scrape({ urls: ['https://bots.discordlabs.org/queue'], directory: './queuescrape/' });
-        resolve(queue);
+        await scrape({ urls: ['https://bots.discordlabs.org/queue'], directory: './queuescrape/', maxDepth: 1 });
+        const queue = fs.readFileSync('./queuescrape/index.html');
+        const root = HTMLParser.parse(queue);
+        const rows = root.querySelectorAll('tr');
+        let botids = [];
+        for (const g of rows) {
+            botids.push(g.getAttribute('id'));
+        }
+        botids.splice(0, 1);
+        resolve(botids);
     });
 }
