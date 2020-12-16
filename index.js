d5db99210b5212188d1c3f410540413b97ba2c8e
BreMea <63671187+BreMea@users.noreply.github.com>
2020-12-15 20:46:12 -0500

    add express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// anish gay
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('fuck!');
+});
+
+app.listen(3000, () => {
+  console.log(`App is live at http://localhost:${PORT}/`);
+});
